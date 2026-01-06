e329ae45fe28a7ca14c0017eb38bf5cd8f47c439
paawanjotk <paawanjotkaur05@gmail.com>
2026-01-06 17:07:30 +0530

    data-packing-memory-bound-exercise-done

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -5,13 +5,13 @@ constexpr int N = 1000000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
+// FIXME: this data structure can be reduced in size -- done
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  long long l:16;
+  int i:8;
+  short s:7;
+  bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
