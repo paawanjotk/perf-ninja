7b745940e6aa638b91225edc254ab6f796925b47
paawanjotk <paawanjotkaur05@gmail.com>
2026-01-06 19:25:21 +0530

    loop-interchange-1-exercise-done

--- labs/memory_bound/loop_interchange_1/solution.cpp
+++ labs/memory_bound/loop_interchange_1/solution.cpp
@@ -25,11 +25,11 @@ void identity(Matrix &result) {
 // Multiply two square matrices
 void multiply(Matrix &result, const Matrix &a, const Matrix &b) {
   zero(result);
-
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      for (int k = 0; k < N; k++) {
-        result[i][j] += a[i][k] * b[k][j];
+    for (int k = 0; k < N; k++) {
+      auto aik = a[i][k];
+      for (int j = 0; j <N; j++) {
+        result[i][j] += aik * b[k][j];
       }
     }
   }
